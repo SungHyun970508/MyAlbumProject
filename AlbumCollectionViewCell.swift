d42ff5d945ea7760dd29d77e51d5cba7d9cf1bea
SungHyun <223602@shinsegae.com>
2022-08-22 17:03:43 +0900

    iOS 3차 수습과제 : MyAlbum - collectionViewCell에 스마트 앨범 fetch해서 대표이미지,앨범이름, 앨범에 포함된 사진 수 뷰에 띄우는 기능까지 완성

---
+++ AlbumCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumCollectionViewCell.swift
+//  MyAlbumPJT
+//
+//  Created by SSG on 2022/08/22.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var albumCoverImage: UIImageView!
+    @IBOutlet var albumName: UILabel!
+    @IBOutlet var photoCount: UILabel!
+    
+}
--- MyAlbumPJT/ViewController.swift
+++ MyAlbumPJT/ViewController.swift
@@ -6,14 +6,93 @@
 //
 
 import UIKit
+import Photos
+
+/*
+ Photos 프레임워크
+ - PHAsset: 사진 라이브러리에 있는 이미지, 비디오와 같은 하나의 애셋을 의미
+ - PHAssetCollection: PHAsset의 컬렉션
+ - PHCachingImageManager: 요청한 크기에 맞게 이미지를 로드하여 캐싱까지 수행
+ - PHFetchResult: 앨범 하나
+ */
 
 class ViewController: UIViewController {
+    
+    //MARK: - Properties
+    let cellIdentifier: String = "albumCollectionCell"
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
+    private var albumCollections = PHFetchResult<PHAssetCollection>()// PHFetchReseult => 배열과 비슷
 
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        requestCollection{
+            granted in guard granted else { return }
+            self.fetchAssets()
+            DispatchQueue.main.async {
+                print("큐에 스레드 돌아가는중")
+                self.collectionView.reloadData()
+            }
+        }
     }
+    
+    func requestCollection(completionHandler: @escaping(Bool) -> Void) {
+        
+        guard PHPhotoLibrary.authorizationStatus() != .authorized else {
+            completionHandler(true)
+            return
+        }
+        
+        PHPhotoLibrary.requestAuthorization { status in completionHandler(status == .authorized )}
+    }
+    
+    func fetchAssets(){
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)] //최신 날짜순으로 사진들 sort
+        //smartAlbum 타입이라서 그런지 옵션으로 최신 날짜순의 정렬을 하면 에러 발생함 options를 nil 로 주어야함.
+        self.albumCollections = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil)
+    }
+}
 
-
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if self.albumCollections.count <= 0 {
+            print("앨범이 제대로 로드되지 않음")
+            return 0
+        } else {
+            return self.albumCollections.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? AlbumCollectionViewCell else {
+            fatalError("Unable to dequeue")
+        }
+        var coverAsset: PHAsset?
+        let collection = albumCollections[indexPath.item]
+        let fetchedAssets = PHAsset.fetchAssets(in: collection, options: nil)
+        coverAsset = fetchedAssets.firstObject
+        
+        cell.albumName.text = collection.localizedTitle
+        cell.photoCount.text = String(fetchedAssets.count)
+        cell.albumCoverImage.image = UIImage(named: "emptyPhoto")
+       
+        let resultHandler: (UIImage?, [AnyHashable: Any]?) -> Void = { image, info in
+            cell.albumCoverImage.image = image
+        }
+        
+        guard let resultAsset = coverAsset else {
+            return cell
+        }
+        imageManager.requestImage(for: resultAsset, targetSize: cell.bounds.size, contentMode: .aspectFill, options: nil, resultHandler: resultHandler)
+        
+        return cell
+    }
+    
+    
+   
 }
 
